95d16103cff0159b24e51f80cccbe8e4c0fd0664
vincent reddy <vincentreddy25@gmail.com>
2026-04-07 16:40:19 +0530

    added useRef

--- myapp/src/components/Hooks3.jsx
+++ myapp/src/components/Hooks3.jsx
@@ -1,8 +1,28 @@
-export default function Hooks3() {
-    return (
-        <>
-            <h3>useRef</h3>
+import { useState, useRef } from "react";
 
-        </>
-    )
+export default function Hooks3() {
+
+    const [num, setNum] = useState(0)
+    const count = useRef(0)
+
+    console.log(" - - - Rendering Component - - - ")
+
+    const increase = () => {
+        count.current += 1
+        console.log("count:", count.current)
+    };
+
+    const inc = () => {
+        setNum(num + 1)
+    }
+
+    return (
+        <div className="border m-4 p-4">
+            <h2>with useRef Count: {count.current}</h2>
+            <button onClick={increase} className="border px-4">Increase useRef</button>
+
+            <h1>num: {num}</h1>
+            <button onClick={inc} className="border px-4">Increase Num</button>
+        </div>
+    );
 }
